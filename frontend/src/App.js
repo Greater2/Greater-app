3fe52552cc27d38164fe7663a263252fb18fe0eb
Greater 2 <greatereze6@gmail.com>
2022-04-26 17:20:18 +0100

    Adding off Loading and Error, connecting backend to frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,65 +1,35 @@
 import React from "react";
-import data from "./data";
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import HomeScreen from "./Screen/HomeScreen";
+import ProductScreen from "./Screen/ProductScreen";
 
 function App() {
   return (
-    <div className='grid-container'>
-      <header className='row'>
-        <div>
-          <a className='brand' href='/'>
-            Greater-shop
-          </a>
-        </div>
-        <div>
-          <a href='/cart'>Cart</a>
-          <a href='/signin'>Sign In</a>
-        </div>
-      </header>
-      <main>
-        <div>
-          <div className='row center'>
-            {data.products.map((product) => {
-              return (
-                <div key={product._id} className='card'>
-                  <a href={`/product/${product._id}`}>
-                    <img
-                      className='medium'
-                      style={{ height: 250 }}
-                      src={product.image}
-                      alt={product.name}
-                    />
-                  </a>
-                  <div className='card-body'>
-                    <a href={`/product/${product._id}`}>
-                      <h2>{product.name}</h2>
-                    </a>
-                    <div className='rating'>
-                      <span>
-                        <i className='fa fa-star'></i>
-                      </span>
-                      <span>
-                        <i className='fa fa-star'></i>
-                      </span>
-                      <span>
-                        <i className='fa fa-star'></i>
-                      </span>
-                      <span>
-                        <i className='fa fa-star'></i>
-                      </span>
-                      <span>
-                        <i className='fa fa-star'></i>
-                      </span>
-                    </div>
-                    <div className='price'>${product.price}</div>
-                  </div>
-                </div>
-              );
-            })}
+   <Router>
+      <div className='grid-container'>
+        <header className='row'>
+          <div>
+            <a className='brand' href='/'>
+              Greater-shop
+            </a>
           </div>
-        </div>
-      </main>
-      <footer className='row center'>All right reserved</footer>
-    </div>
+          <div>
+            <a href='/cart'>Cart</a>
+            <a href='/signin'>Sign In</a>
+          </div>
+        </header>
+        <main>
+          <Routes>
+          <Route path="/" element={<HomeScreen/>} exact />
+          <Route path="/product/:id" element={<ProductScreen/>} />
+
+
+          </Routes>
+          
+        </main>
+        <footer className='row center'>All right reserved</footer>
+      </div>
+      </Router>
   );
 }
 
